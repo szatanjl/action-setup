48ade6f0ba6a25aa8c2bca223a6be44c7869e851
szatan <szatan@lszj.org>
2023-10-01 11:02:12 +0200

    test...

---
+++ dist/main/index.js
@@ -0,0 +1,14 @@
+#!/usr/bin/env node
+/******/ (() => { // webpackBootstrap
+/******/ 	/* webpack/runtime/compat */
+/******/ 	
+/******/ 	if (typeof __nccwpck_require__ !== 'undefined') __nccwpck_require__.ab = __dirname + "/";
+/******/ 	
+/************************************************************************/
+var __webpack_exports__ = {};
+
+console.log("Hello world!");
+
+module.exports = __webpack_exports__;
+/******/ })()
+;
